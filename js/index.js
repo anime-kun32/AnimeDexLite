e1ed5f2988001f4f88e9c231486212514a0dd876
Adedoyin <160499103+anime-kun32@users.noreply.github.com>
2024-09-12 12:49:39 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const recentapi = "/recent/";
 
 // Api Server Manager
 
-const AvailableServers = ["https://api100.anime-dex.workers.dev"];
+const AvailableServers = ["https://anime-dex-api-urcc.vercel.app"];
 
 function getApiServer() {
     return AvailableServers[Math.floor(Math.random() * AvailableServers.length)];
